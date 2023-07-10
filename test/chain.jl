62fe26706013e2bc7bbdd8fd5b94d0ea95e2a2bf
Scott Jordan <scott.m.jordan91@gmail.com>
2023-07-09 19:02:01 -1000

    fixed unit tests

--- test/chain.jl
+++ test/chain.jl
@@ -6,21 +6,29 @@ using DecisionMakingEnvironments
 
 @testset "Simple Chain Tests" begin
     prob = simple_chain(3; stochastic=false, failchance=0.1, randomize=false)
-    @test typeof(prob) <: MDP
+    @test typeof(prob) <: SequentialProblem
     @test length(prob.A) == 2
     @test eltype(prob.A) <: Int
     @test length(prob.S) == 3
     @test eltype(prob.S) <: Int
-    @test prob.d0() isa Int
-    @test prob.d0() == 1
-    @test prob.p(prob.d0(),2) isa Int
-    @test prob.p(1,2) == 2
-    @test prob.p(3,2) == 1
-    @test prob.r(1,2,1) isa Float64
-    @test prob.r(1,2,1) == -1.0
-    @test prob.γ(prob.d0(),2,1) isa Float64
-    @test prob.γ(3,1,2) == 1.0
-    @test prob.γ(3,2,1) == 0.0
+    @test prob.d0() isa Tuple{Int,Int}
+    @test prob.d0() == (1,1)
+    @test prob.p(prob.d0()[1],2)[1] isa Int
+    @test prob.p(prob.d0()[1],2)[2] isa Int
+    @test prob.p(prob.d0()[1],2)[3] isa Float64
+    @test prob.p(prob.d0()[1],2)[4] isa Float64
+    for s in prob.S
+        for a in prob.A
+            @test prob.p(s,a) == sample(prob, s, a)
+        end
+    end
+    @test prob.p(1,2)[1] == 2
+    @test prob.p(3,2)[1] == 1
+    @test prob.p(1,2)[3] == -1.0
+    @test prob.p(1,2)[4] == 1.0
+    @test prob.p(3,2)[4] == 0.0
+    @test prob.p(1,1)[1] == 1
+
     meta = meta_information(prob)
     @test meta[:minreturn] isa Float64
     @test meta[:minreward] == -1
@@ -28,7 +36,7 @@ using DecisionMakingEnvironments
     @test meta[:minhorizon] == 3
     @test meta[:maxhorizon] == Inf
     prob = simple_chain(3, stochastic=true)
-    @test prob.p(1,2) isa Int
+    @test prob.p(1,2)[1] isa Int
 end
 
 @testset "Simple Chain Finitetime Tests" begin
